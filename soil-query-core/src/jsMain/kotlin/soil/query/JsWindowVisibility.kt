74e2d81da86039cb3a37da0fe9ef6abfad851816
TBSten <programmingCafeteria@gmail.com>
2025-08-28 12:15:12 +0900

    Copy the wasmJsMain code to jsMain

---
+++ soil-query-core/src/jsMain/kotlin/soil/query/JsWindowVisibility.kt
@@ -0,0 +1,41 @@
+// Copyright 2024 Soil Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package soil.query
+
+import kotlinx.browser.document
+import org.w3c.dom.events.Event
+import soil.query.core.WindowVisibility
+import soil.query.core.WindowVisibilityEvent
+import soil.query.core.document as documentAlt
+
+/**
+ * Implementation of [WindowVisibility] for Js.
+ */
+class JsWindowVisibility : WindowVisibility {
+
+    private var visibilityListener: ((Event) -> Unit)? = null
+
+    override fun addObserver(observer: WindowVisibility.Observer) {
+        visibilityListener = {
+            if (documentAlt.visibilityState == VISIBILITY_STATE_VISIBLE) {
+                observer.onReceive(WindowVisibilityEvent.Foreground)
+            } else {
+                observer.onReceive(WindowVisibilityEvent.Background)
+            }
+        }
+        document.addEventListener(TYPE_VISIBILITY_CHANGE, visibilityListener)
+    }
+
+    override fun removeObserver(observer: WindowVisibility.Observer) {
+        visibilityListener?.let { document.removeEventListener(TYPE_VISIBILITY_CHANGE, it) }
+        visibilityListener = null
+    }
+
+    companion object {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Document/visibilitychange_event
+        @Suppress("SpellCheckingInspection")
+        private const val TYPE_VISIBILITY_CHANGE = "visibilitychange"
+        private const val VISIBILITY_STATE_VISIBLE = "visible"
+    }
+}
